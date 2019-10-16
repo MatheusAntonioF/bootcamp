307229a774f07d926e1277c59919f7cf7743d09b
MockingWarChief <matheusantoniof@gmail.com>
2019-10-16 15:03:09 -0300

    Criação e edição de usuários

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -7,22 +7,18 @@ export default async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return res.status(401).json({ error: 'Token not provided ' });
+    return res.status(401).json({ error: 'Token not provided' });
   }
 
-  // desestrutura um array e obtem a segunda posição
-  // método split gera um array
   const [, token] = authHeader.split(' ');
 
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
-    console.log(decoded);
+    req.userId = decoded.id;
 
     return next();
   } catch (err) {
     return res.status(401).json({ error: 'Token invalid' });
   }
-
-  return next();
 };
