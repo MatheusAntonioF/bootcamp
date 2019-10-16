c849561b7933dbf1c54b82b4b2c25d9b7a976ab7
MockingWarChief <matheusantoniof@gmail.com>
2019-10-16 18:23:31 -0300

    Upload de imagem

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -2,6 +2,7 @@ import File from '../models/File';
 
 class FileController {
   async store(req, res) {
+    // desestrutura array
     const { originalname: name, filename: path } = req.file;
 
     const file = await File.create({
